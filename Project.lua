531a2d13096fdd249cdd83b09083f7579f81d0e6
rrhiemstar <rrhiemstar@gmail.com>
2024-02-28 06:36:27 +0100

    <dep> added PkgB v0.1.0 as a dependency

--- Project.lua
+++ Project.lua
@@ -1,8 +1,10 @@
 Project = {
-    name = "PkgA",
-    uuid = "508eb781-a825-4a62-a371-5d4d066e288c",
-    authors = {"rrhiemstar<rrhiemstar@gmail.com>"},
-    version = "0.1.0",
-    deps = {}
+  name = "PkgA",
+  uuid = "508eb781-a825-4a62-a371-5d4d066e288c",
+  authors = {"rrhiemstar<rrhiemstar@gmail.com>", }, 
+  version = "0.1.0",
+  deps = {
+        PkgB = "0.1.0",
+    },
 }
 return Project
