9788268568975724af81af6ec8de87f5aa296382
rrhiemstar <rrhiemstar@gmail.com>
2024-02-28 06:40:08 +0100

    <release> PkgA..v0.2.0

--- Project.lua
+++ Project.lua
@@ -2,7 +2,7 @@ Project = {
   name = "PkgA",
   uuid = "508eb781-a825-4a62-a371-5d4d066e288c",
   authors = {"rrhiemstar<rrhiemstar@gmail.com>", }, 
-  version = "0.1.0",
+  version = "0.2.0",
   deps = {
         PkgB = "0.2.0",
     },
