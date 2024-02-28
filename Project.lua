4192e54dd49c9f016243267abf7368097bc93e3e
rrhiemstar <rrhiemstar@gmail.com>
2024-02-28 06:24:45 +0100

    <new package> PkgA

---
+++ Project.lua
@@ -0,0 +1,8 @@
+Project = {
+    name = "PkgA",
+    uuid = "508eb781-a825-4a62-a371-5d4d066e288c",
+    authors = {"rrhiemstar<rrhiemstar@gmail.com>"},
+    version = "0.1.0",
+    deps = {}
+}
+return Project
